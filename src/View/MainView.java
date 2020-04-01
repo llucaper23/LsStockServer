4e2aa5601644dbc6edc81f07e85f1ee4c023018b
albert <albert.lalmolda@students.salle.url.edu>
2020-04-01 12:29:42 +0200

    config finestra

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -10,47 +10,47 @@ public class MainView extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(MAXIMIZED_BOTH);
         setVisible(true);
-        setTitle("Ejemplo JTabbedPane");
+        setTitle("LS - Stocks");
 
-        //Creamos el conjunto de pestañas
-        JTabbedPane pestañas=new JTabbedPane();
-
-        //Creamos el panel y lo añadimos a las pestañas
-        JPanel panel1=new JPanel();
-
-        //Componentes del panel1
-        JLabel et_p1=new JLabel("Estas en el panel 1");
-        panel1.add(et_p1);
+        //fem les pestanyes --> TabbedPane
+        JTabbedPane jTPpestanyes=new JTabbedPane();
 
 
-        //Añadimos un nombre de la pestaña y el panel
-        pestañas.addTab("Panel 1", panel1);
+        // Creem els diferents panells que els afegirem com a pestanyes
 
-        //Realizamos lo mismo con el resto
-        JPanel panel2=new JPanel();
-        pestañas.addTab("Panel 2", panel2);
+        // Pestanya Acciones per user
+            JPanel jpAccionsXUser=new JPanel();
 
-        //Componentes del panel2
-        JLabel et_p2=new JLabel("Estas en el panel 2");
-        panel2.add(et_p2);
+            //continguts del panell
+            JLabel jlAccionsxUser=new JLabel("Accions X Usuari text");
+            jpAccionsXUser.add(jlAccionsxUser);
+            //l'afegim
+            jTPpestanyes.addTab("Accions X Usuari", jpAccionsXUser);
 
-        JPanel panel3=new JPanel();
 
-        //Componentes del panel3
-        JLabel et_p3=new JLabel("Estas en el panel 3");
-        panel3.add(et_p3);
+        //Pestanya Top 10
+            JPanel panel2=new JPanel();
 
-        pestañas.addTab("Panel 3", panel3);
+            //continguts del panell
+            JLabel et_p2=new JLabel("Estas en el panel 2");
+            panel2.add(et_p2);
 
-        JPanel panel4=new JPanel();
+            jTPpestanyes.addTab("Panel 2", panel2);
 
-        //Componentes del panel4
-        JLabel et_p4=new JLabel("Estas en el panel 4");
-        panel4.add(et_p4);
 
-        pestañas.addTab("Panel 4", panel4);
 
-        getContentPane().add(pestañas);
+
+
+        //Pestanya 3
+            JPanel panel3=new JPanel();
+             //continguts del panell
+            JLabel et_p3=new JLabel("Estas en el panel 3");
+            panel3.add(et_p3);
+
+            jTPpestanyes.addTab("Panel 3", panel3);
+
+        // ho posem al contenidor general
+        getContentPane().add(jTPpestanyes);
 
 
 
