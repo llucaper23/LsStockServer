aa9b8329a65753c5fd4132134a4c157abd590012
pablocarol <47698187+pablocarol@users.noreply.github.com>
2020-04-15 23:00:35 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Controller.MainViewController;
+import Controller.UserShareViewController;
+import Model.Network.Network;
+import View.MainView;
+
 public class Main {
     public static void main(String[] args) {
+        MainView mainView = new MainView();
+        Network network = new Network();
 
+        MainViewController mvc = new MainViewController(mainView, network);
     }
 }
