1005973939348cc303fd0558eecf25578590783e
pablocarol <47698187+pablocarol@users.noreply.github.com>
2020-04-15 23:01:25 +0200

    Add files via upload

--- src/Model/Network/Network.java
+++ src/Model/Network/Network.java
@@ -1,4 +1,49 @@
 package Model.Network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Network {
+    private static final int PORT = 34568;
+    private ServerSocket serverSocket;
+
+    private ArrayList<NetworkThread> networkThreadList;
+    private boolean isRunning;
+
+    public Network() {
+        networkThreadList = new ArrayList<>();
+        serverSocket = null;
+    }
+
+    public void run() {
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            isRunning = true;
+
+            while (isRunning) {
+                System.out.println("Waiting for a client...");
+                Socket socket = serverSocket.accept();
+
+                NetworkThread networkThread = new NetworkThread(socket, networkThreadList);
+                networkThreadList.add(networkThread);
+                networkThread.start();
+                System.out.println("Client connected");
+
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
 }
--- src/Model/Network/NetworkThread.java
+++ src/Model/Network/NetworkThread.java
@@ -1,5 +1,29 @@
 package Model.Network;
 
+import java.io.IOException;
+
 public class NetworkThread extends Thread {
+
+    @Override
+    public void run () {
+        try {
+            while (true) {
+
+            }
+        } catch (IOException e) {
+
+        } finally {
+            try {
+
+            } catch (IOException e) {
+
+            }
+            try {
+
+            } catch (IOException e) {
+            }
+        }
+
+    }
 
 }
