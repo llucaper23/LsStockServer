57820000ba02049c153ad81342541705f859b9bf
pablocarol <47698187+pablocarol@users.noreply.github.com>
2020-04-16 12:10:27 +0200

    dbconnector working :)

--- src/Model/Network/NetworkConfiguration.java
+++ src/Model/Network/NetworkConfiguration.java
@@ -3,7 +3,7 @@ package Model.Network;
 public class NetworkConfiguration {
 
     // constants relacionades amb la comunicacio
-    public static final String SERVER_IP = "127.0.0.1";
+    public static final String SERVER_IP = "localhost";
     public static final int SERVER_PORT = 34567;
     public static final String DB_ADDRESS = "lsstock-database.mysql.database.azure.com";
     public static final String DB_USER = "ls_stock@lsstock-database";
