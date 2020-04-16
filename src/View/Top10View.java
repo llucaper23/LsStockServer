d46f661130a3b09bb4c3db10a8ae0a1b249f5eca
albert <albert.lalmolda@students.salle.url.edu>
2020-04-17 01:38:56 +0200

    Afegeixo grafic de barres fet amb Swing i awt en comptes de JFreeChar Top10View sera l'encarregada de fer X crides en el seu for per a poder crear les barretes(Top 10 View es el panell, i HistogramPanel es cada Barra)

--- src/View/Top10View.java
+++ src/View/Top10View.java
@@ -3,16 +3,26 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Top10View extends JPanel {
 
     final int MAX_HEIGHT = 700;
     final int MAX_WIDTH = 1250;
 
     public Top10View() {
-        JPanel jpGraficas =new JPanel();
 
+        // farem el set dels valors que tinguem en la nostre BD --> son inventants , tocara canviarlos despres per les dades reals, s'ha d'eliinar la classe
+        HistogramPanel panel = new HistogramPanel();
+
+        // agefim les 10 comapnyies !!!WARNING nomes esta dimensionat per 10, si els volguessin posat mes o reduir tocaraia cnviar la capçalera de HistogrmPanel
+        for (int i = 0; i < 10; i++) {
+
+            panel.addHistogramColumn("Telefonica" + i, i * 10, Color.RED);
+        }
+
+        panel.layoutHistogram();
+
+        this.add(panel);
     }
-
-
 
 }
