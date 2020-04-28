b194cf6a7c9dcbfa34527186bd4a7fce9b8bef4a
albert <albert.lalmolda@students.salle.url.edu>
2020-04-28 23:21:52 +0200

    preparo vista accions usuari amb el controller i els listenners a l'espera de dades a la BBDD.

--- src/Controller/UserShareViewController.java
+++ src/Controller/UserShareViewController.java
@@ -1,14 +1,27 @@
 package Controller;
 
+import Model.CompanyiesModel;
+import View.UserShareView;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UserShareViewController implements ActionListener {
 
 
+    private UserShareView userShareWindow;
+
+    public UserShareViewController(UserShareView userShareWindow) {
+        this.userShareWindow = userShareWindow;
+    }
 
     @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getActionCommand().equals("x")){  // caldra comprovar amb el nom de tots els usuaris que tenim, sera el Action listener que li haurem posat
+            // haurem de fer un set a les dades que ens mostrara la vista per pantalla->
+
+        }
 
     }
 }
--- src/Model/CompanyiesModel.java
+++ src/Model/CompanyiesModel.java
@@ -1,4 +1,4 @@
-package View;
+package Model;
 
 public class CompanyiesModel {
 
