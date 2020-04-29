a66285d3dfa465604b8a79ad1e134106b8cfe16a
pablocarol <47698187+pablocarol@users.noreply.github.com>
2020-04-29 02:48:46 +0200

    implementada funcio updateAllUsers i updateAllCompanies en temps real

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import Controller.MainViewController;
 import Controller.Top10ViewController;
 import Model.Network.Server;
 import View.MainView;
+import View.Top10View;
 
 public class Main {
     public static void main(String[] args) {
-        MainView mainView = new MainView();
+        Top10View top10View = new Top10View();
+        MainView mainView = new MainView(top10View);
         Server server = new Server();
         server.startServer();
         MainViewController mvc = new MainViewController(mainView, server);
