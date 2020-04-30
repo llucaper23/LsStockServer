834125b31bafa5f4564f05103a88d04df27b87e6
llucaper23 <llucaper@gmail.com>
2020-05-01 00:50:00 +0200

    modificacions BBDD i getAllCompanies

--- src/Model/Database/DAO/BotDAO.java
+++ src/Model/Database/DAO/BotDAO.java
@@ -1,4 +1,58 @@
 package Model.Database.DAO;
 
+import Model.Bot;
+import Model.Database.DBConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
 public class BotDAO {
+
+    public void insertBot(Bot bot){
+        try {
+            String query = "INSERT INTO BOT (buy_percentage, activation_time, company_id) VALUES (" + bot.toString() + ");";
+            DBConnector.getInstance().insertQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Bot> getAllBots(){
+        try{
+            String query = "SELECT * FROM BOT;";
+            ResultSet rs = DBConnector.getInstance().selectQuery(query);
+            ArrayList<Bot> bots = new ArrayList<>();
+            while (rs.next()){
+                int bot_id = rs.getInt("bot_id");
+                float buy_percentage = rs.getFloat("buy_percentage");
+                float activation_time = rs.getFloat("activation_time");
+                int company_id = rs.getInt("company_id");
+                bots.add(new Bot(bot_id,buy_percentage,activation_time,company_id));
+            }
+            return bots;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ArrayList<Bot> getAllBotsFromCompany(int company_id){
+        try{
+            String query = "SELECT * FROM BOT WHERE company_id = " + company_id + ";";
+            ResultSet rs = DBConnector.getInstance().selectQuery(query);
+            ArrayList<Bot> bots = new ArrayList<>();
+            while (rs.next()){
+                int bot_id = rs.getInt("bot_id");
+                float buy_percentage = rs.getFloat("buy_percentage");
+                float activation_time = rs.getFloat("activation_time");
+                bots.add(new Bot(bot_id,buy_percentage,activation_time,company_id));
+            }
+            return bots;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
